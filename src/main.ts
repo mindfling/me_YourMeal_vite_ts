e1b2aad84c92ad95ac36db051c984d5b4f24a744
Nepochtov Dmitry <78114857+mindfling@users.noreply.github.com>
2025-07-07 20:52:03 +0300

    try some logo and buttons

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,33 @@
-import './style.css'
-import typescriptLogo from './typescript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.ts'
+import './style.css';
+import typescriptLogo from './typescript.svg';
+import javascriptLogo from './javascript.svg';
+import viteLogo from '/vite.svg';
+import { setupCounter } from './counter.ts';
 
-document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
+const appdiv = document.querySelector<HTMLDivElement>('#app');
+console.log('appdiv: ', appdiv, typeof appdiv);
+
+const amountH2 = document.querySelector('#amount');
+amountH2!.textContent = 'simple 1'
+console.log('amountDiv: ', amountH2);
+
+let summ: number = 0;
+
+function setupCounter(element: HTMLButtonElement | null) {
+  if (element) {
+    let counter = 0;
+    const setCounter = (count: number) => {
+      counter = count;
+      element.textContent = `count is ${counter}`;
+      amountH2!.textContent = (summ + counter).toString();
+    }
+    element.addEventListener('click', () => setCounter(counter + 1))
+    setCounter(0)
+  }
+}
+
+
+/* !.innerHTML = `
   <div>
     <a href="https://vitejs.dev" target="_blank">
       <img src="${viteLogo}" class="logo" alt="Vite logo" />
@@ -19,6 +43,9 @@ document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
       Click on the Vite and TypeScript logos to learn more
     </p>
   </div>
-`
+`;
+*/
+// setupCounter(document.querySelector<HTMLButtonElement>('#counter')!);
 
-setupCounter(document.querySelector<HTMLButtonElement>('#counter')!)
+const buttonOne = document.querySelector<HTMLButtonElement>('#counterOne');
+setupCounter(buttonOne);
