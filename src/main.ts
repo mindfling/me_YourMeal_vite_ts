059c5867587e2dfea23d7620c9292e67b1816210
Nepochtov Dmitry <78114857+mindfling@users.noreply.github.com>
2025-07-18 23:22:45 +0300

    feat header footer

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,12 @@
-import './style.css';
+// import './style.css';
 import viteLogo from '/vite.svg';
 
 const app = () => {
   console.log('Hallo ts', viteLogo);
+  
+  const text: string = 'Только самые сочные бургеры!';
+  const siteTitle = document.querySelector('#site-title');
+  siteTitle!.textContent = text;
 }
 
 
